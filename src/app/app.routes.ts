0e6a1e318a02a9f3de91fc822967d8e15a98ba07
Ayush Tripathi <tripathi.at02@gmail.com>
2024-12-05 22:46:41 +0530

    routes edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'app-home', component: HomeComponent },
+    { path: 'app-registration', component:RegistrationComponent},
+    { path: 'app-login', component:LoginComponent},
+    { path: '', redirectTo: '/app-home', pathMatch: 'full' }
+];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  searchBusDto = {
+    origin: '',
+    destination: '',
+    travelDate: ''
+  };
 
+  searchResults: any[] = [];
+
+  searchBus(form: any) {
+    // Mocking search results
+    this.searchResults = [
+      {
+        busName: 'Express Bus',
+        departureTime: new Date('2024-12-04T10:00:00'),
+        arrivalTime: new Date('2024-12-04T14:00:00'),
+        fare: 300
+      },
+      {
+        busName: 'City Link',
+        departureTime: new Date('2024-12-04T11:00:00'),
+        arrivalTime: new Date('2024-12-04T15:30:00'),
+        fare: 350
+      }
+    ];
+  }
+
+  viewBusDetails(bus: any) {
+    console.log('Viewing details for:', bus);
+  }
 }
