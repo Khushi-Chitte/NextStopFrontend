cc1d1c41fb41c426f98fc2a0efb5ba5f2fc1f69f
Ayush Tripathi <tripathi.at02@gmail.com>
2024-12-08 20:52:59 +0530

    modified api service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { RegistrationComponent } from './components/registration/registration.co
 import { LoginComponent } from './components/login/login.component';
 import { ViewresultsComponent } from './components/viewresults/viewresults.component';
 import { BookBusComponent } from './components/book-bus/book-bus.component';
-import { BusComponent } from './components/bus/bus.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
 import { ConfirmBookingComponent } from './components/confirm-booking/confirm-booking.component';
@@ -19,7 +18,6 @@ export const routes: Routes = [
     { path: 'app-login', component: LoginComponent },
     { path: 'app-viewresults', component: ViewresultsComponent },
     { path: 'app-book-bus', component: BookBusComponent },
-    { path: 'app-bus', component: BusComponent },
     { path: 'app-profile', component: ProfileComponent },
     { path: 'app-update-profile', component: UpdateProfileComponent },
     { path: 'app-confirm-booking', component: ConfirmBookingComponent },
